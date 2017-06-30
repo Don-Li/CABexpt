90ae196ca6c473525a360a60aa29198e75207f7b
Don-Li <yli877@aucklanduni.ac.nz>
2017-06-30 16:58:05 +1200

    Revert "test folder and init"
    This reverts commit fc3bd67487fab155a16b02ec24688c92c609a2f3.

--- python/__init__.py
+++ python/__init__.py
@@ -4,8 +4,5 @@
 import event_record
 import input_outputs
 import clock
-import directory_setup
-import dropbox_download
-import temperature
 
 # __all__ is a list that contains all modules to be imported with *
