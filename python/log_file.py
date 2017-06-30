6fcf184b85d9c210e4ec76ddaab95d378defc83e
Don-Li <yli877@aucklanduni.ac.nz>
2017-06-30 18:24:21 +1200

    log file

---
+++ python/log_file.py
@@ -0,0 +1,28 @@
+# Functions for the log file
+
+import os
+import time
+
+def update_logs( time, entry ):
+    log_file = open( "/home/pi/Experiment/log.txt", "a" )
+    log_message = "%s,%s" % (time, entry)
+    log_file.write( log_message )
+    log_file.close()
+
+def make_new_log():
+    directory = "/home/pi/Experiment/"
+    log_file_name = "%slog.txt" % (directory)
+    log_file = open( log_file_name, "w" )
+    log_file.close()
+
+def start_new_log():
+    directory = "/home/pi/Experiment/"
+    log_directory = "%slog.txt" % (directory)
+    
+    date = time.strftime( "%H_%M_%d_%m_%Y" )
+
+    name_for_old_log = "%slog_ended_%s" % (directory, date)
+
+    os.rename( log_directory, name_for_old_log )
+    make_new_log()
+
