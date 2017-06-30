47c69d6b916b9c73510500eecd90a54f484e7624
Don-Li <yli877@aucklanduni.ac.nz>
2017-06-30 17:00:16 +1200

    Tests and init
    Moved timing test to tests folder
    Updated __init__

--- python/__init__.py
+++ python/__init__.py
@@ -4,5 +4,8 @@
 import event_record
 import input_outputs
 import clock
+import directory_setup
+import dropbox_download
+import temperature
 
 # __all__ is a list that contains all modules to be imported with *
