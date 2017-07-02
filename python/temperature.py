4ede35158c01fe3398e35ba85ef390990180704b
Don Li <yli877@aucklanduni.ac.nz>
2017-07-02 17:13:10 +1200

    Updated temperature function
    Uses subprocess module instead of os

--- python/temperature.py
+++ python/temperature.py
@@ -1,6 +1,7 @@
-import os
+from subprocess import check_output
 
 read_temp = function():
     command = "/opt/vc/bin/vcgencmd measure_temp"
-    temp = os.popen(command).readlines()
+    temp = check_output( command, shell = True, universal_newlines = True )
+    temp = temp.strip( "\n" )
     return( temp )
