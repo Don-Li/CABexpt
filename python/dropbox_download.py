850b5ab53bb3900684e821ab78d8b4af50461dc2
Don Li <yli877@aucklanduni.ac.nz>
2017-07-03 21:15:40 +1200

    Dropbox upload/download
    dropbox_download now uses subpocess.check_output and prints the script call
    dropbox_download also has param_download for downloading parameters
    
    dropbox_upload now imports check_output from subprocess

--- python/dropbox_download.py
+++ python/dropbox_download.py
@@ -1,21 +1,24 @@
-import os
+# Dropbox downloader
+
+import time
+from log_file import update_logs
+from subprocess import check_output
 
 
-
-def dropbox_download( remote_file_directory, local_directory ):
-
+def dropbox_download( remote_file_path, local_directory ):
     uploader_script = "dropbox_uploader download"
-
-
+    
     if not os.path.exists( local_directory ):
-
         raise NameError( "Local directory not found" )
 
-
     os_call = "%s %s %s" % (uploader_script, remote_file_directory, local_directory)
-
-    print( os_call )
+    os_print = check_output( os_call, shell = True, universal_newlines = True )
+    print( os_print )
 
     os.system( os_call )
+def param_download( param_file ):
+    local_directory = "/home/pi/Experiment"
+    remote_directory = ""
+    file_path = "%s%s" % ( remote_directory, param_file )
 
-    print( "File downloaded" )
+    dropbox_download( remote_file_path, local_directory )
