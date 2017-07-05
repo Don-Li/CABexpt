62595312215e89ff4d06048ee87a90e4749d75bd
Don Li <yli877@aucklanduni.ac.nz>
2017-07-06 10:17:37 +1200

    File readers
    file_reader has a function read_one_col_csv() for reading csv files
    read_experiment() and read_identity() now uses read_one_col_csv()

---
+++ python/file_reader.py
@@ -0,0 +1,7 @@
+# File reader
+
+def read_one_col_csv( file_path ):
+	with open( file_path, "r" ) as file:
+		read_file = csv.reader( file, delimiter = "," )
+		return_dict = { row[0].strip(" "): row[1].strip(" ") for row in read_file }
+	return( return_dict )
