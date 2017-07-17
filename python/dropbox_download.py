fcc33b48b21554d1b403dc4aa5d3a6f247521373
Don Li <yli877@aucklanduni.ac.nz>
2017-07-17 15:54:21 +1200

    dropox_uploader, experiment_reader, identity, setup
    dropbox_download now has full script for dropbox uploader
    experiment_reader has a function to clone the experiment folder in Dropbox
    identity now has a docstring
    setup.py fixed spelling for description

--- python/dropbox_download.py
+++ python/dropbox_download.py
@@ -3,12 +3,16 @@
 import time
 from log_file import update_logs
 from subprocess import check_output
+import clock
 
 
 def dropbox_download( remote_file_path, local_directory ):
-    uploader_script = "dropbox_uploader download"
+    uploader_script = "/home/pi/Dropbox-Uploader/dropbox_uploader.sh download"
     
     if not os.path.exists( local_directory ):
+        error_message = "Cannot download " + remote_file_path
+        date = clock.get_date_hm()
+        log_file.update_logs( date, error_message )
         raise NameError( "Local directory not found" )
 
     os_call = "%s %s %s" % (uploader_script, remote_file_directory, local_directory)
@@ -16,6 +20,7 @@ def dropbox_download( remote_file_path, local_directory ):
     print( os_print )
 
     os.system( os_call )
+    
 def param_download( param_file ):
     local_directory = "/home/pi/Experiment"
     remote_directory = ""
--- python/identity.py
+++ python/identity.py
@@ -3,5 +3,9 @@ import csv
 import file_reader
 
 def read_identity():
+    """
+    Returns a dictionary
+    """
+    
 	file_path = "/home/pi/Experiment/identity.txt"
 	return( file_reader.read_one_col_csv( file_path ) )
