6dadb75fa82815a0d5a05df0d4b9c35b298970b0
Don Li <yli877@aucklanduni.ac.nz>
2017-10-26 20:45:20 +1300

    Experiment reader
    Added "read_experiment_params()" to find and make a parameter dictionary
    Changed "log_file.py" so that it makes a new line after an entry

--- CABexpt/experiment_reader.py
+++ CABexpt/experiment_reader.py
@@ -3,12 +3,13 @@
 import csv
 import file_reader
 import os
-import dropbox_download
+import log_file
+import clock
 from glob import glob
 
 def read_experiment( dirctory ):
     """
-    Return a dictionary
+    Read a CSV file and return a dictionary of parameters
     """
     files = os.chdir( directory )
     # .expt is the file extension for the experiment details
@@ -23,3 +24,45 @@ def clone_experiment_folder( directory ):
         file_to_download = "/experiment" + file_names[file]
         dropbox_download.dropbox_download( file_to_download, "/home/pi/Experiment" )
     os.chdir( original_wd )
+
+def read_experiment_params( file_name, subject, session ):
+    """ Read a CSV file and return a dictionary of parameters """
+    
+    with open( file_name, mode = "r" ) as file:
+        csv_file= csv.reader( file, delimiter = "," )
+        
+        # First row is always a header
+        # Always has "subject" and "session"
+        header = csv_file.__next__()
+        try:
+            subject_column = header.index( "subject" )
+        except ValueError:
+            msg = " 'subject' column not found"
+            print( msg )
+            log_file.update_log( time = clock.get_date_hmdmy(), entry = msg )
+        try:
+            session_column = header.index( "session" )
+        except ValueError:
+            msg = " 'session' column not found"
+            print( msg )
+            log_file.update_log( time = clock.get_date_hmdmy(), entry = msg )
+        
+        # Find columns for parameters
+        parameter_cols = [ x for x in range( len(header) ) if x not in [subject_column, session_column] ]
+        parameter_names = [ header[x] for x in parameter_cols ]
+        
+        parameter_dict = {}
+        
+        for row in csv_file:
+            if row[ subject_column ] == subject and row[ session_column ] == session:
+                parameter_dict[ "subject" ] = row[ subject_column ]
+                parameter_dict[ "session" ] = row[ session_column ]
+                for params in range( len(parameter_cols) ):
+                    # Record log files
+                    value = row[params]
+                    parameter_name = parameter_names[params]
+                    parameter_dict[ parameter_name ] = value
+                    msg = " subject %s, session %s, parameter %s, value %s" % (subject, session, parameter_name, value )
+                    log_file.update_log( time = clock.get_date_hmdmy(), entry = msg )
+    
+    return( parameter_dict )
